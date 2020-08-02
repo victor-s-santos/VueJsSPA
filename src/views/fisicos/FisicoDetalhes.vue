715286b314f6428c5caceb13a0b80e8c6d17c9e1
victor-s-santos <victor.s.silva@unesp.br>
2020-08-02 17:31:39 -0300

    investigando os parâmetros da rota

--- src/views/fisicos/FisicoDetalhes.vue
+++ src/views/fisicos/FisicoDetalhes.vue
@@ -1,3 +1,11 @@
 <template>
-    <h3 class="font-weight-light">Detalhes sobre o Físico:</h3>
+    <h3 class="font-weight-light">Detalhes sobre o Físico de id: {{$route.params.id}}</h3>
 </template>
+
+<script>
+export default {
+    created(){
+        console.log(`Parâmetros da rota: ${this.$route.params}`)
+    }
+}
+</script>
