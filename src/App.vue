d28228c76984315584f3309a81ca87a5e4b6ea8d
victor-s-santos <victor.s.silva@unesp.br>
2020-08-02 15:39:58 -0300

    navegação entre rotas utilizando router link

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
     </div>
 
     <div class="container">
+
+      <router-link to='/' class='btn btn-info mb-2 mr-2'>Página Inicial</router-link>
+      <router-link :to='{path: "/fisicos"}' class='btn btn-info mb-2 mr-2'>Físicos Importantes</router-link>
       <router-view></router-view>
     </div>
     
